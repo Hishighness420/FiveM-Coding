c5ee92e420f9f6ea31f93acc163fcb04a510db9a
Hishighness420 <misters79@protonmail.com>
2019-05-11 11:08:42 -0300

    Delete client.lua

--- client.lua
+++
@@ -1,6 +0,0 @@
---This function /xyz returns the coordinates the player is currently occupying.
-
-RegisterCommand("xyz", function(source, args, rawCommand)
-    coords = GetEntityCoords(PlayerPedId())
-	TriggerEvent("chatMessage", "coordinates are " .. coords)
-end)
