af78e225c96cf4f40e968b6e2dc0a391b07a3063
Hishighness420 <misters79@protonmail.com>
2019-05-18 08:55:28 -0300

    Add files via upload

---
+++ defiant-economy/fivem.sql
@@ -0,0 +1,38 @@
+-- phpMyAdmin SQL Dump
+-- version 4.6.5.2
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: May 18, 2019 at 01:54 PM
+-- Server version: 10.3.14-MariaDB
+-- PHP Version: 5.6.30
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `fivem`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `economy`
+--
+
+CREATE TABLE `economy` (
+  `id` varchar(255) DEFAULT NULL,
+  `cash` int(50) DEFAULT NULL,
+  `bank` int(50) DEFAULT NULL,
+  `dirtymoney` int(50) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
