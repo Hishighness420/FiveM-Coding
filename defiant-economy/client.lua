b6ecb2a7e5aadd6efd7ef513d7223b56c5814376
Hishighness420 <misters79@protonmail.com>
2019-05-18 08:47:49 -0300

    Create client.lua

---
+++ defiant-economy/client.lua
@@ -0,0 +1,6 @@
+--MYSQL Database functions
+
+# RegisterNetEvent("output")
+# AddEventHandler("output", function(argument)
+#    TriggerEvent("chatMessage", "[Success]", {0,255,0}, argument)
+# end)
